7dd307d96e62ba4460239a790e9c5b2d8648ae04
Tyler <tylerlantern@gmail.com>
2017-07-07 23:13:36 +0700

    Initial Commit

---
+++ CustomControlTransitionDelegate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomControlTransitionDelegate
+//
+//  Created by Nattapong Unaregul on 7/7/17.
+//  Copyright © 2017 Nattapong Unaregul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
