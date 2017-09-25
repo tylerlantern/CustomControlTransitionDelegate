56cc4bf23ac75ac708f24f61f30ec43f795bc3d4
To <nattapong.u@23perspective.com>
2017-09-25 14:13:28 +0700

    interactive, presentation transition

---
+++ CustomControlTransitionDelegate/SvView.swift
@@ -0,0 +1,21 @@
+//
+//  SvView.swift
+//  CustomControlTransitionDelegate
+//
+//  Created by Nattapong Unaregul on 8/21/17.
+//  Copyright © 2017 Nattapong Unaregul. All rights reserved.
+//
+
+import UIKit
+
+class SvView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
